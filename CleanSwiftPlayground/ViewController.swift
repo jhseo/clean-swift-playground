0b68eda3d18ac78a803faa3872a7f696bf5d904b
Junghun Seo <sjh7893@gmail.com>
2020-04-27 10:21:33 +0900

    add default project

---
+++ CleanSwiftPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftPlayground
+//
+//  Created by jhseo on 2020/04/27.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
